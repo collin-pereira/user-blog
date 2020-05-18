d4383b46ece8ca40549d10364b362654213c62fa
Dominic Fernandes <dominic.fernandes@CreativeCapsule.local>
2020-05-18 11:25:44 +0530

    cleaned the code for update user profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -72,41 +72,16 @@ const deleteUser = async (req, res) => {
 }
 
 const updateProfileImage = async (req, res) => {
+    const busboy = new Busboy({ headers: req.headers, limits: { fileSize: 50000, files: 1 } })
     let { id } = req.params
     try {
         let exists = await helper.resourceExists(User, id)
         if (!exists) return res.status(404).send(NOT_FOUND)
+        saveImage(busboy, req, res)
+        req.pipe(busboy)
     } catch (error) {
         res.status(500).send(error)
     }
-    const busboy = new Busboy({ headers: req.headers, limits: { fileSize: 50000, files: 1 } })
-    var imageName, imagePath
-
-    busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
-        imageName = `${Date.parse(new Date())}_${filename}`
-        imagePath = `${__BASEDIR}${process.env.STATIC}/${imageName}`
-        file.on('limit', () => {
-            res.status(413).send({ message: "file size exceeded, max: 5MB" })
-        })
-        file.pipe(fs.createWriteStream(imagePath));
-    })
-
-    busboy.on('finish', async () => {
-        try {
-            let imageLink = await getUserImageLink(req.params.id)
-            await User.update({ image_link: "http://localhost:4000/user/uploads/" + imageName }, { where: { id: id } })
-            if (imageLink !== null) {
-                fs.unlinkSync(getImagePath(imageLink))
-            }
-            res.status(200).send(SUCCESS)
-        } catch (error) {
-            console.log(error);
-            
-            res.status(500).send(error)
-        }
-    })
-
-    req.pipe(busboy)
 }
 
 const deleteProfileImage = async (req, res) => {
@@ -124,6 +99,30 @@ const deleteProfileImage = async (req, res) => {
     }
 }
 
+const saveImage = (busboy, req, res) => {
+    var imageName, imagePath
+    busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+        imageName = `${Date.parse(new Date())}_${filename}`
+        imagePath = `${__BASEDIR}${process.env.STATIC}/${imageName}`
+        file.on('limit', () => {
+            res.status(413).send({ message: "file size exceeded, max: 5MB" })
+        })
+        file.pipe(fs.createWriteStream(imagePath));
+    })
+
+    busboy.on('finish', async () => {
+        try {
+            let imageLink = await getUserImageLink(req.params.id)
+            await User.update({ image_link: "http://localhost:4000/user/uploads/" + imageName }, { where: { id: req.params.id } })
+            if (imageLink !== null) {
+                fs.unlinkSync(getImagePath(imageLink))
+            }
+            res.status(200).send(SUCCESS)
+        } catch (error) {
+            res.status(500).send(error)
+        }
+    })
+}
 const getUserImageLink = async (id) => {
     try {
         let { image_link } = await User.findByPk(id, { attributes: ['image_link'] })
